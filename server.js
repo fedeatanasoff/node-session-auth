a948146091d2534cd3ad1e2a595cbd8baa8ba51f
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-01 20:14:55 -0300

    registrando las sessiones en mongo con connect mongo

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 const app = express();
 const port = process.env.PORT || 2000;
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/auth";
 
 app.use(
   session({
     secret: "secreto!",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: new MongoStore({
+      url: MONGO_URL,
+      autoReconnect: true
+    })
   })
 );
 
 app.get("/", (req, res) => {
-  req.session.cuenta = req.session.cuenta
-    ? req.session.cuenta + 1
-    : 1;
+  req.session.cuenta = req.session.cuenta ? req.session.cuenta + 1 : 1;
   res.send(
-    `hello friend. has visitado esta pagina ${
-      req.session.cuenta
-    } veces`
+    `hello friend. has visitado esta pagina ${req.session.cuenta} veces`
   );
 });
 
-app.listen(port, () =>
-  console.log(`servidor corriendo en el puerto ${port}`)
-);
+app.listen(port, () => console.log(`servidor corriendo en el puerto ${port}`));
