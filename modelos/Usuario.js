fc0e021fa2f40da5e2c42857632b67afe13ccd0c
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-02 01:56:40 -0300

    utilizando bcryp para encriptar la constraseña

--- modelos/Usuario.js
+++ modelos/Usuario.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const usuarioSchema = new Schema(
   {
@@ -11,3 +12,29 @@ const usuarioSchema = new Schema(
     timestamps: true
   }
 );
+
+usuarioSchema.pre("save", function(next) {
+  const usuario = this;
+  // cambio la contraseña?
+  if (!usuario.isModified("password")) {
+    return next();
+  }
+
+  //   genero una salt
+  bcrypt.genSalt(10, (error, salt) => {
+    if (err) {
+      next(err);
+    }
+
+    // utilizo la salt y genero una ueva contraseña
+    // a traves del hash y sustituyo
+    bcrypt.hash(usuario.password, salt, null, (err, hash) => {
+      if (err) {
+        next(err);
+      }
+
+      usuario.password = hash;
+      next();
+    });
+  });
+});
